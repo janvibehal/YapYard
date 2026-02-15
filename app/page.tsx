23398e1313c5faf0c10993a74dc131f6d52a8b88
janvibehal <janvibehal2@gmail.com>
2026-02-15 19:15:06 +0530

    fixed scrollers

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,12 @@ import MiddleContent from "../components/posts/MiddleContent";
 import Navbar from "../components/navigation/Navbar";
 
 export default function Home() {
+
+  const hideScrollbar = {
+    scrollbarWidth: "none" as const,
+    msOverflowStyle: "none" as const,
+  };
+
   return (
     <div className="min-h-screen bg-gray-50 dark:bg-[#0a0a0a]">
 
@@ -16,7 +22,10 @@ export default function Home() {
       <div className="flex pt-14">
 
         {/* LEFT SIDEBAR */}
-        <div className="hidden md:block">
+        <div
+          className="hidden md:block sticky top-14 h-[calc(100vh-3.5rem)] overflow-y-auto [&::-webkit-scrollbar]:hidden"
+          style={hideScrollbar}
+        >
           <SidebarLeft />
         </div>
 
@@ -25,8 +34,11 @@ export default function Home() {
           <MiddleContent />
         </div>
 
-        <div className="hidden lg:block">
-          {/* RIGHT SIDEBAR */}
+        {/* RIGHT SIDEBAR */}
+        <div
+          className="hidden lg:block sticky top-14 h-[calc(100vh-3.5rem)] overflow-y-auto [&::-webkit-scrollbar]:hidden"
+          style={hideScrollbar}
+        >
           <SidebarRight />
         </div>
 
